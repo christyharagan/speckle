e1e29bf6edb97ac9669cf3baef9a7b1c179601a8
Christy Haragan <christy@christy.haragan@gmail.com>
2015-07-14 16:00:19 +0100

    Add 'and' keyword to query builder

--- lib/queryBuilder.ts
+++ lib/queryBuilder.ts
@@ -4,29 +4,40 @@ import {PrefixBuilder} from './prefix'
 
 export interface Query {
   toSparql(): string
+  and(src: Value, pred: Value, obj: Value): Query
 }
 
 export interface Select {
   where(src: Value, pred: Value, obj: Value): Query
 }
 
-export function select(...variables:Variable[]): Select {
-  let prefixBuilder = new PrefixBuilder()
-  let query = 'SELECT '
-  variables.forEach(function(variable){
-    query += `?${variable.name} `
-  })
+function _where(variables:Variable[], prefixBuilder:PrefixBuilder, statements:[Value, Value, Value][], src: Value, pred: Value, obj: Value): Query {
+  prefixBuilder = prefixBuilder.addValue(src).addValue(pred).addValue(obj)
+  statements.push([src, pred, obj])
   return {
-    where: function(src: Value, pred: Value, obj: Value): Query {
-      query = query + `WHERE {
- ${v(src)} ${v(pred)} ${v(obj)};
-}`
-      query = prefixBuilder.addValue(src).addValue(pred).addValue(obj).toSparql(query)
-      return {
-        toSparql(): string {
-          return query
-        }
+    toSparql(): string {
+      let query = 'SELECT '
+      variables.forEach(function(variable){
+        query += `?${variable.name} `
+      })
+      query += `WHERE {
+`
+      for (let i = 0; i < statements.length - 1; i++) {
+        let statement = statements[i]
+        query += `${v(statement[0])} ${v(statement[1])} ${v(statement[2])} .
+`
       }
-    }
+      let statement = statements[statements.length - 1]
+      query += `${v(statement[0])} ${v(statement[1])} ${v(statement[2])};
+}`
+      return prefixBuilder.toSparql(query)
+    },
+    and: _where.bind(null, variables, prefixBuilder, statements)
+  }
+}
+
+export function select(...variables:Variable[]): Select {
+  return {
+    where: _where.bind(null, variables, new PrefixBuilder(), [])
   }
 }
